a4248a0c02c9d01079024dfc5a70f881f483ae6d
Timothy jan <jan.timothy@gmail.com>
2023-02-27 14:54:11 -0800

    Created Service

--- EmployeeManagementApp/src/app/app-routing.module.ts
+++ EmployeeManagementApp/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddEmployeeComponent } from './add-employee/add-employee.component';
-import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
-import { HomeComponent } from './home/home.component';
-import { ViewEmployeeComponent } from './view-employee/view-employee.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewEmployeeComponent } from './components/view-employee/view-employee.component';
 
 const routes: Routes = [
   { path: "", redirectTo: 'Home', pathMatch: 'full' },
--- EmployeeManagementApp/src/app/app.module.ts
+++ EmployeeManagementApp/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AddEmployeeComponent } from './add-employee/add-employee.component';
-import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
-import { ViewEmployeeComponent } from './view-employee/view-employee.component';
+import { HomeComponent } from './components/home/home.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { ViewEmployeeComponent } from './components/view-employee/view-employee.component';
 
 @NgModule({
   declarations: [
